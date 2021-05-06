2780409a17f5fe817a0e98ca708a81a76838bd8d
marianaknust <77455775+marianaknust@users.noreply.github.com>
2021-05-06 11:57:49 +0200

    Fixed time format

--- package.js
+++ package.js
@@ -1,66 +1,32 @@
-// let weather = {
-//   "paris": {
-//     temp: "19°C (66°F)",
-//     humidity: 80
-//   },
-//   "tokyo": {
-//     temp: "17.3°C (63.14°F)",
-//     humidity: 50
-//   },
-//   "lisbon": {
-//     temp: "30.2°C (86.33°F)",
-//     humidity: 20
-//   },
-//   "san francisco": {
-//     temp: "20.9°C (69.62°F)",
-//     humidity: 100
-//   },
-//   "moscow": {
-//     temp: "-5°C (23°F)",
-//     humidity: 20
-//   }
-// };
-
-//   let enterCity = prompt("Enter city");
-//         enterCity = enterCity.trim().toLowerCase();
-//         let city = weather[enterCity];
-//         function capitalizeFirstLetter(city) {
-//                 return city.charAt(0).toUpperCase() + city.slice(1);
-//             }
-
-//         if (city) {
-//         enterCity = capitalizeFirstLetter(enterCity); 
-//             alert(`It is currently ${city.temp} in ${enterCity} with a humidity of ${city.humidity}%`);
-//         } else {
-//             alert(`Sorry, we don't know the weather for this city, try going to https://www.google.com/search?q=weather+${enterCity}`);
-//         }
-
-
-
 let now = new Date();
 let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 let currentDay = days[now.getDay()];
-let currentHour = now.getHours();
-let currentMinute = now. getMinutes();
+let hours = now.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+let minutes = now. getMinutes();
+ if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
 let today = now.getUTCDate ();
 let months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Set","Oct","Nov","Dec"];
 let currentMonth = months[now.getMonth()];
 
 
 let currentDate = document.querySelector("#current-date");
-currentDate.innerHTML = `${currentDay}, ${today} ${currentMonth} ${currentHour}:${currentMinute}`;
+currentDate.innerHTML = `${currentDay}, ${today} ${currentMonth} ${hours}:${minutes}`;
 
 let apiKey = "05f78a209463d416f8843b75229cbdc0";
 
 function searchCity (event) {
   event.preventDefault();
-
+  
   let chosenCity = document.querySelector(".insert-city").value;
-
-
+  
   let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${chosenCity}&appid=${apiKey}&units=metric`;
-
-   axios.get(apiURL).then(displayTemperature);
+  
+  axios.get(apiURL).then(displayTemperature);
 }
 
 function searchCurrent (position) {
